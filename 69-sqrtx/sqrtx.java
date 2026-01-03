2077b688418926913f6160355736d4d6d11f9940
Rajini <chekurirajini485@gmail.com>
2026-01-03 12:34:54 +0530

    Time: 1 ms (96.22%) | Memory: 42.5 MB (78.08%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,33 +1,27 @@
 class Solution {
     public int mySqrt(int x) {
-        // For special cases when x is 0 or 1, return x.
-        if (x == 0 || x == 1)
-            return x;
+        
+        int si = 0;
+        int ei = x;
 
-        // Initialize the search range for the square root.
-        int start = 1;
-        int end = x;
-        int mid = -1;
+        int ans = 0;
 
-        // Perform binary search to find the square root of x.
-        while (start <= end) {
-            // Calculate the middle point using "start + (end - start) / 2" to avoid integer overflow.
-            mid = start + (end - start) / 2;
+        while(si <= ei) {
+            int mid = ei + (si - ei) / 2;
+            long temp = (long) mid * mid;
 
-            // If the square of the middle value is greater than x, move the "end" to the left (mid - 1).
-            if ((long) mid * mid > (long) x)
-                end = mid - 1;
-            else if (mid * mid == x)
-                // If the square of the middle value is equal to x, we found the square root.
+            if(temp == x) {
                 return mid;
-            else
-                // If the square of the middle value is less than x, move the "start" to the right (mid + 1).
-                start = mid + 1;
+            }
+            else if(temp < x) {
+                si = mid + 1;
+                ans = mid;
+            }
+            else {
+                ei = mid - 1;
+            }
         }
 
-        // The loop ends when "start" becomes greater than "end", and "end" is the integer value of the square root.
-        // However, since we might have been using integer division in the calculations,
-        // we round down the value of "end" to the nearest integer to get the correct square root.
-        return Math.round(end);
+        return ans;
     }
 }
