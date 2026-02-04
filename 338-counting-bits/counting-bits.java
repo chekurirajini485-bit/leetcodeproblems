a93e57c50a8ce5236525dae1d468ad2c787dc7b6
Rajini <chekurirajini485@gmail.com>
2026-02-04 18:33:05 +0530

    Time: 2 ms (96.46%) | Memory: 48.7 MB (65.63%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] arr=new int[n+1];
+        arr[0]=0;
+        for(int i=1;i<=n;i++){
+
+           // arr[i]=arr[((2<<i)-1)+1];
+            arr[i] = arr[i >> 1] + (i & 1);
+
+        }
+        return arr;
+    }
+}
